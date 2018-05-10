0a81ae7009e64486b6dd62af0c45f0377a9df360
Allan Baptista <allan.marques@ymail.com>
2018-05-09 22:46:12 -0300

    Adding jsx

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,16 @@
+/* @jsx createElement */
+
 import { createElement, render } from './tiny-react';
 
 const rootElement = document.getElementById('root');
 
-const jsonDomTree = createElement(
-    'div',
-    { onclick: () => alert('Clicked'), className: 'container' },
-    createElement(
-        'h1',
-        { className: 'main-title' },
-        'This is a title'
-    ),
-    createElement(
-        'div',
-        { className: 'main-article' },
-        createElement(
-            'p',
-            { id: 'paragraph-one' },
-            'This is some paragraph text'
-        ),
-    ),
+const jsonDomTree = (
+    <div className="container" onClick={() => alert('Clicked')}>
+        <h1 className="main-title">This is a title</h1>
+        <div className="main-article">
+            <p id="paragraph-one">This is some paragraph text</p>
+        </div>
+    </div>
 );
 
 render(jsonDomTree, rootElement);
